9a5aa00acd4ebf11c46cb4930ef31ae289b0cd50
Nathan Muir <nathan.muir@3stack.com>
2017-06-23 10:52:17 +1000

    CLI - move common options to parent cli command

--- nv/cli.py
+++ nv/cli.py
@@ -10,37 +10,39 @@ logger = logging.getLogger(__name__)
 
 
 @click.group()
-def main():
-    pass
-
-
-@main.command('create')
-@click.argument('environment_name', default='', type=_valid_environment_name)
-@click.option('--project-name', '-p', default=None,
-              help='''Your project name (defaults to current directory name)''')
+@click.option('--environment-name', '-n', default='', type=_valid_environment_name)
 @click.option('--project-dir', '-d', default='.',
               type=click.Path(file_okay=False, dir_okay=True, exists=True),
               help='''Path to the project project (defaults to current directory)''')
+@click.option('wants_password', '-P', default=False, is_flag=True)
+@click.option('use_keyring', '-K', default=False, is_flag=True)
+@click.pass_context
+def main(ctx, environment_name, project_dir, wants_password, use_keyring):
+    password = None
+    if wants_password:
+        password = click.prompt('Password', hide_input=True)
+    ctx.obj = (environment_name, project_dir, password, use_keyring)
+
+
+@main.command('create')
+@click.option('--project-name', '-p', default=None,
+              help='''Your project name (defaults to current directory name)''')
 @click.option('--python-virtualenv', '-py', is_flag=True, default=False,
               help='''Activate a python virtualenv''')
 @click.option('--aws-profile', default=None,
               help='''Obtain credentials for the given profile.''')
 @click.option('environment_vars', '--env', type=(unicode, unicode), multiple=True)
-@click.option('wants_password', '-P', default=False, is_flag=True)
-@click.option('use_keyring', '-K', default=False, is_flag=True)
-def cmd_create(environment_name, project_name, project_dir, python_virtualenv, aws_profile, environment_vars, wants_password, use_keyring):
+@click.pass_context
+def cmd_create(ctx, project_name, python_virtualenv, aws_profile, environment_vars):
     """Create a new environment in %PROJECT%/.nv-%ENVIRONMENT_NAME%"""
-    password = None
-    if wants_password:
-        password = click.prompt('Password', hide_input=True)
-
+    environment_name, project_dir, password, use_keyring = ctx.obj
     nv_dir = create(
         project_dir, environment_name, project_name=project_name, python_virtualenv=python_virtualenv,
         aws_profile=aws_profile, environment_vars=dict(environment_vars), password=password, use_keyring=use_keyring
     )
     rel_dir = os.path.relpath(nv_dir, os.getcwd())
     click.echo("""
-environment created at {0}.
+environment created at "{0}"
 Launch with:
 \tnv shell {1}
 
@@ -49,29 +51,19 @@ Specify extra environment variables by editing {0}/environment.json
 
 
 @main.command('rm')
-@click.argument('environment_name', default='', type=_valid_environment_name)
-@click.option('--project-dir', '-d', default='.',
-              type=click.Path(file_okay=False, dir_okay=True, exists=True),
-              help='''Path to the project project (defaults to current directory)''')
-def cmd_remove(environment_name, project_dir):
+@click.pass_context
+def cmd_remove(ctx):
     """Remove an environment."""
+    environment_name, project_dir = ctx.obj[:2]
     click.echo("Removing {0} in {1}".format(environment_name, project_dir))
     remove(project_dir, environment_name)
 
 
 @main.command('shell')
-@click.argument('environment_name', default='', type=_valid_environment_name)
-@click.option('--project-dir', '-d', default='.',
-              type=click.Path(file_okay=False, dir_okay=True, exists=True),
-              help='''Path to the project project (defaults to current directory)''')
-@click.option('wants_password', '-P', default=False, is_flag=True)
-@click.option('update_keyring', '-K', default=False, is_flag=True)
-def cmd_shell(environment_name, project_dir, wants_password, update_keyring):
+@click.pass_context
+def cmd_shell(ctx):
     """Launch a new shell in the specified environment."""
+    environment_name, project_dir, password, use_keyring = ctx.obj
     click.echo("Launching nv subshell. Type 'exit' or 'Ctrl+D' to return.")
-    password = None
-    if wants_password:
-        password = click.prompt('Password', hide_input=True)
-
-    launch_shell(project_dir, environment_name, password, update_keyring)
+    launch_shell(project_dir, environment_name, password, use_keyring)
     click.echo('Environment closed.')
